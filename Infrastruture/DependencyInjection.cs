90c031582d9edfbc13ba214b092cb70258f7ba67
Kevin Noviski <kevin.noviski@gmail.com>
2024-08-10 23:39:11 -0300

    primeiro commit

---
+++ Infrastruture/DependencyInjection.cs
@@ -0,0 +1,10 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastruture;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastruture(this IServiceCollection services)
+    {
+        return services;
+    }
+}
